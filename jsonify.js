057af61a874b00343e2625e168924095a2bbae11
Piper Chester <piperchester@gmail.com>
2019-04-28 11:14:50 -0700

    chore: delete cents.json

---
+++ jsonify.js
@@ -0,0 +1,14 @@
+// Simple script to take a JS file and export a JSON file
+// so that VSCE can package it up
+const fs = require('fs');
+const centsJson = 'cents.json';
+
+// Delete previous if exists
+if (fs.existsSync(centsJson)) {
+  console.log(`Deleting old ${centsJson}...`);
+  fs.unlinkSync(centsJson);
+}
+
+const theme = require('./cents.js');
+fs.writeFileSync(centsJson, JSON.stringify(theme));
+console.log('Written ', centsJson);
